93b38b341bbd428d1703f025258c4940391f10a8
typing-guoruncheng <guoruncheng@typing.email>
2026-03-26 18:22:28 +0800

    feat:vercel

--- next.config.ts
+++ next.config.ts
@@ -2,24 +2,6 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   serverExternalPackages: ["pdf-parse", "pdfjs-dist"],
-  async headers() {
-    return [
-      {
-        source: "/(.*)",
-        headers: [
-          {
-            key: "X-Frame-Options",
-            value: "ALLOW-FROM https://person-blog-sage.vercel.app",
-          },
-          {
-            key: "Content-Security-Policy",
-            value:
-              "frame-ancestors 'self' https://person-blog-sage.vercel.app http://localhost:*",
-          },
-        ],
-      },
-    ];
-  },
 };
 
 export default nextConfig;
